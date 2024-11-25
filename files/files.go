595af9c75cbc120e06fb17c761750259c334d45b
Daniel Hermel <danielhermel.oliveira@gmail.com>
2024-11-25 17:24:51 -0300

    Siguiente Commit

---
+++ files/files.go
@@ -0,0 +1,61 @@
+package files
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/Eng-Daniel-Hermel/godesde0/ejercicios"
+)
+
+var fileName string = "./files/txt/tabla.txt"
+
+func GravarTabla() {
+	var texto string = ejercicios.TabladeMultiplicar()
+	archivo, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("Error al crear el archivo" + err.Error())
+		return
+	}
+	fmt.Fprintln(archivo, texto)
+	archivo.Close()
+}
+
+func SumaTabla() {
+	var texto string = ejercicios.TabladeMultiplicar()
+	if !Append(fileName, texto) {
+		fmt.Println("Error al concatenar contenido")
+	}
+}
+
+func Append(filen string, texto string) bool {
+	arch, err := os.OpenFile(fileName, os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Println("Error durante el Append" + err.Error())
+		return false
+	}
+
+	_, err = arch.WriteString(texto)
+	if err != nil {
+		fmt.Println("Error al concatenar WriteString" + err.Error())
+		return false
+	}
+
+	arch.Close()
+	return true
+}
+
+func LeoArchivo() {
+	archivo, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Hubo un error leyendo el archivo " + err.Error())
+		return
+	}
+
+	scanner := bufio.NewScanner(archivo)
+	for scanner.Scan() {
+		registro := scanner.Text()
+		fmt.Println("> " + registro)
+	}
+	archivo.Close()
+}
--- main.go
+++ main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/Eng-Daniel-Hermel/godesde0/variables"
+	"github.com/Eng-Daniel-Hermel/godesde0/files"
 )
 
 func main() {
-	estado, texto := variables.ConviertoaTexto(1588)
+	/*estado, texto := variables.ConviertoaTexto(1588)
 	fmt.Println(estado)
 	fmt.Println(texto)
+	if os := runtime.GOOS; os=="linux" || os=="OS X." {
+		fmt.Println("Esto no es Windows, es", os)
+	} else {
+		fmt.Println("Esto es", os)
+	}
+
+	switch os := runtime.GOOS; os  {
+	case "linux":
+		fmt.Println("Esto es", os)
+	case "darwin":
+		fmt.Println("Esto es", os)
+	default:
+		fmt.Printf("%s \n", os)
+	}
+
+	numero, texto := ejercicios.ConvNumerico("ffffff")
+	fmt.Println(numero)
+	fmt.Println(texto
+
+	teclado.IngresoNumeros()
+
+	iteraciones.Iterar()
+
+	fmt.Println(ejercicios.TabladeMultiplicar())*/
+
+	//files.GravarTabla()
+
+	//files.SumaTabla()
+
+	files.LeoArchivo()
 }
